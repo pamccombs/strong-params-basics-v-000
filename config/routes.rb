49b680fdeb2c0d0c3c41b707cc9f84170c6ec56e
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-24 07:45:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  patch 'posts/:id', to: 'posts#update'
+  #patch 'posts/:id', to: 'posts#update'
   resources :posts, only: %i[index show new create edit update]
 end
